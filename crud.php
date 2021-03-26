1ff39ab9d3989da39b0ab401010bbaf8af8e5e8d
Eduardo <16128000+carliedu@users.noreply.github.com>
2021-03-26 18:32:03 -0300

    Create crud.php

---
+++ crud.php
@@ -0,0 +1,26 @@
+<?php
+	date_default_timezone_set('America/Sao_Paulo');
+	error_reporting(E_ALL);
+	echo("\n\n\n\n\n\n\n\n");
+	echo(date("d/m/Y-G:i:s")." (crud.php) INI---INI---INI---INI---INI---INI---INI---INI---INI---INI---INI---\n");
+
+	use React\EventLoop\Factory;
+	use src\dbClass;
+
+	require 'vendor/autoload.php';
+	
+	$loop = Factory::create();
+	
+		echo(date("d/m/Y-G:i:s")." (crud.php) Started Eventloop\n");
+		
+		$db = new dbClass($loop);
+	
+		$UserData = $db->getUser("user1");
+		print_r($UserData);
+	
+		echo(date("d/m/Y-G:i:s")." (crud.php) Connection-quit\n\n\n");
+		$connection->quit();
+		$db->quit();
+	$loop->run();
+	echo(date("d/m/Y-G:i:s")." (crud.php) Ended Eventloop. Bye bye\n");
+?>
